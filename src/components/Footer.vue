c4d7f70ab1985e5714406bbff61c9f3b6b3279ec
Franco Giordani <franco@space.bar>
2020-09-05 22:53:01 -0500

    email

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -42,7 +42,7 @@
     <div class="Contact">
       <h2 class="title">Contact</h2>
       <p>
-        <a href="mailto:francogiordani@gmail.com" target="_blank">Email me</a>
+        <a href="mailto:francogiordanid@gmail.com" target="_blank">Email me</a>
       </p>
     </div>
   </div>
