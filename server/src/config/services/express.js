06840cca2d79a82cec7da2ab5b457441decadab1
Franco G <franco@space.bar>
2020-12-12 03:03:50 -0600

    rate limiting

--- server/src/config/services/express.js
+++ server/src/config/services/express.js
@@ -5,6 +5,22 @@ const show = require('./logging')
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 10 minutes
+  max: 100
+})
+
+const corsOptions = {
+  origin: true,
+  'Access-Control-Allow-Methods': 'GET',
+  'Access-Control-Allow-Credentials': true,
+  'Access-Control-Allow-Origin': true,
+  'Access-Control-Allow-Headers': true,
+  'Access-Control-Expose-Headers': true,
+  credentials: true
+}
 
 /**
  * Express configuration
@@ -12,15 +28,7 @@ const cors = require('cors')
  */
 const init = () => {
   const app = express()
-  const corsOptions = {
-    origin: true,
-    'Access-Control-Allow-Methods': 'GET',
-    'Access-Control-Allow-Credentials': true,
-    'Access-Control-Allow-Origin': true,
-    'Access-Control-Allow-Headers': true,
-    'Access-Control-Expose-Headers': true,
-    credentials: true
-  }
+  app.set('trust proxy', 1)
   app.use('/static', express.static(config.clientStaticFolder))
   app.use('/', express.static(config.clientBuildFolder))
   app.set('views', config.clientBuildFolder)
@@ -28,6 +36,7 @@ const init = () => {
   app.set('view engine', 'html')
   app.use(cors(corsOptions))
   app.use(helmet())
+  app.use('/api/', apiLimiter)
   app.use('/api', (req, res, next) => {
     show.info(`/api/${req.path} requested`)
     next()
